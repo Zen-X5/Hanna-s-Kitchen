66e7d7bcaffcaf6611b855ee0ceb95236fe4ca80
Sahid-Ahmed111 <sahidahmed7262@gmail.com>
2025-06-17 19:03:20 +0530

    Added _redirects file to support SPA routing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,           // This makes the dev server accessible over the network
-    port: 5173,           // Optional: specify a fixed port (default is 5173)
-  },
+    host: true,
+    port: 5173,
+    proxy: {
+      // Useful only in local development to avoid CORS
+      '/api': {
+        target: 'http://localhost:8080', // your local backend
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
