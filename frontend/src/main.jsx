3b9c2ee4fed8be773d7e23a6bb64a47ba44c5583
Sahid-Ahmed111 <sahidahmed7262@gmail.com>
2025-06-17 19:37:42 +0530

    Added _redirects file to support SPA routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/admin" element={<AdminPanel />} />
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
