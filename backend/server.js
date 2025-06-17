e2a760b7611f5fdd6bcab327a8b639f09e43129b
Sahid-Ahmed111 <sahidahmed7262@gmail.com>
2025-06-17 15:10:26 +0530

    Added _redirects file to support SPA routing

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require("path");
+
 
 const  menuRoutes = require('./routes/menuRoutes');
 const orderRoutes = require('./routes/orderRoutes');
@@ -12,6 +14,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 app.use('/uploads', express.static('uploads'));
 app.use(cors({
@@ -28,6 +31,10 @@ mongoose.connect(process.env.MONGO_URL)
 })
 .catch(err => console.error('MongoDB error:', err));
 
+// Fallback to index.html for React Router
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 
 app.listen(port,()=>{
     console.log(`Server is live at port ${port}`);
